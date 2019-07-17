2add6dcd11a26d39088f35d3197b6deed3e05526
pavlushka66 <pasha@knopka.com>
2019-07-17 08:45:01 +0500

    Some inits

---
+++ src/jiraFormattedReactElement.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export default class JIraFormattedReactElement extends React.Component {
+    render () {
+        return (
+          <div>
+              Empty
+          </div>
+        );
+      }
+}
